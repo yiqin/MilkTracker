d8a84db8f0c28607a49227cd39311442eb993667
Yi Qin <yiqin.mems@gmail.com>
2015-05-11 15:16:35 -0500

    Initial Commit

---
+++ MilkTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MilkTracker
+//
+//  Created by Yi Qin on 5/11/15.
+//  Copyright (c) 2015 Yi Qin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
